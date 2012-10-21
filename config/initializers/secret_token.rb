7efa09d6c1dde51eea839dddb27612d1e2e380e6
Liy <liy8272@gmail.com>
2012-10-21 17:13:25 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NibIo::Application.config.secret_token = '6f5313203990f822a8411046b9977a029e22e7c02899417fc6a9662036d51281d3f4be46f11340e36bad908c6c144de48a8b64ed666e818c11df1dc0d953b5c3'
