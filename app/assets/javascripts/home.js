842c52373bf484996ebb1f54925eff7b826a9bf8
Liy <liy8272@gmail.com>
2012-12-28 23:50:04 +0000

    working on home page

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,5 @@
+// window.onload = function(){
+//   var homeContainer = document.getElementById("home-container");
+//   var height = document.offsetHeight;
+//   homeContainer.style.height = height + 'px';
+// };
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,20 +3,20 @@ class HomeController < ApplicationController
     # if user is signed in, go to his own home page
     if current_user
         redirect_to current_user
+    else
+        # get the rss information
+        require 'simple-rss'
+        require 'open-uri'
+        require 'iconv'
+
+
+        # force to read rss as UTF-8
+        i = Iconv.new('UTF-8', 'UTF-8')
+        # Seems like there is a bug on bitbucket's rss feed. If you try to open the feed it will auto detect the
+        # encoding as western ISO-8859-1, but it is actually is in UTF-8 encoding
+        @rss = SimpleRSS.parse i.iconv(open('https://github.com/liy/nib.io/commits/master.atom').read)
+
+        render :layout => "home"
     end
-
-    # get the rss information
-    require 'simple-rss'
-    require 'open-uri'
-    require 'iconv'
-
-
-    # force to read rss as UTF-8
-    i = Iconv.new('UTF-8', 'UTF-8')
-    # Seems like there is a bug on bitbucket's rss feed. If you try to open the feed it will auto detect the
-    # encoding as western ISO-8859-1, but it is actually is in UTF-8 encoding
-    @rss = SimpleRSS.parse i.iconv(open('https://github.com/liy/nib.io/commits/master.atom').read)
-
-    # if user is not signed in, go to public home page
   end
 end
