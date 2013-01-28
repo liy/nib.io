c9aec50d19e591c7e49a732214f2acd4051eddb3
Liy <liy8272@gmail.com>
2013-01-28 21:31:54 +0000

    Add split related script.

---
+++ app/assets/javascripts/tofu/main.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+  // TODO: only enable it when it is in writing and reading mode
+
+
+// // clean up the html
+// document.body.innerHTML = document.body.innerHTML.replace(/\n|\s\s/g, "");
+
+// var _selection = window.getSelection();
+// var _range = document.createRange();
+// var _ranger = new LineRanger();
+
+// var _splitManager = SplitManager.getInstance();
+// _splitManager.init(_ranger);
+
+// var papers = document.getElementsByClassName("paper");
+
+// var serializer = new JSONSerializer();
+// serializer.init();
+// var obj = serializer.serialize(papers[0]);
+// console.dir(obj);
+};
