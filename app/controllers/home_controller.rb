d1f24bc5c16571ad0b3e7eafb93c16a3c7e03c03
Liy <liy8272@gmail.com>
2013-02-04 22:50:59 +0000

    fix ajax like and subscribe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,8 +12,6 @@ class HomeController < ApplicationController
 
         # force to read rss as UTF-8
         i = Iconv.new('UTF-8', 'UTF-8')
-        # Seems like there is a bug on bitbucket's rss feed. If you try to open the feed it will auto detect the
-        # encoding as western ISO-8859-1, but it is actually is in UTF-8 encoding
         @rss = SimpleRSS.parse i.iconv(open('https://github.com/liy/nib.io/commits/master.atom').read)
 
         render :layout => "home"
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20130115212735) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
+    t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
